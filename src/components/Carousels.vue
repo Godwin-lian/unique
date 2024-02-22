1f2a3c15305c2ca1e9c581b1750b7d22e18c0217
Godwin-lian <godwintindaon2@gmail.com>
2024-02-22 13:24:19 +0700

    update

--- src/components/Carousels.vue
+++ src/components/Carousels.vue
@@ -5,23 +5,23 @@
     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
          <!-- Item 1 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img loading="lazy" src="/src/assets/img/Gemini_Generated_Image (2).jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img loading="lazy" src="/src/assets/img/totebag-2.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="tote-bag">
         </div>
         <!-- Item 2 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img loading="lazy" src="/src/assets/img/Gemini_Generated_Image (5).jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img loading="lazy" src="/src/assets/img/topi.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="topi">
         </div>
         <!-- Item 3 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img loading="lazy" src="/src/assets/img/unnamed (3).png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img loading="lazy" src="/src/assets/img/handuk.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="handuk">
         </div>
         <!-- Item 4 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img loading="lazy" src="/src/assets/img/unnamed (5).png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img loading="lazy" src="/src/assets/img/payung-lipat.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="payung-lipat">
         </div>
         <!-- Item 5 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img loading="lazy" src="/src/assets/img/Gemini_Generated_Image (9).jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img loading="lazy" src="/src/assets/img/tas.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="set-tas">
         </div>
     </div>
 
