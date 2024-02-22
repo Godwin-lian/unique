a2157302fb870c0937ec4a9c79d588fd9f66bffa
Godwin-lian <godwintindaon2@gmail.com>
2024-02-22 14:21:24 +0700

    update

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -7,105 +7,205 @@
         </div>
   
         <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-          <div v-for="product in products" :key="product.id" class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
-            <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
-          New
-        </div>
-            <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
-              <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
-            </div>
-            <div class="flex flex-1 flex-col space-y-2 p-4">
-              <h3 class="text-sm font-medium text-gray-900">
-                <a :href="product.href">
-                  <span aria-hidden="true" class="absolute inset-0" />
-                  {{ product.name }}
-                </a>
-              </h3>
-              <p class="text-sm text-gray-500">{{ product.description }}</p>
-              <div class="flex flex-1 flex-col justify-end">
-                <p class="text-sm italic text-gray-500">{{ product.options }}</p>
-              </div>
-            </div>
-          </div>
+          <!-- Product 1 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg" alt="Delapan kaus tersusun di atas meja dalam warna hitam, zaitun, abu-abu, biru, putih, merah, kuning mustard, dan hijau." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Basic Tee 8-Pack
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Dapatkan rangkaian lengkap Basic Tees kami. Miliki kaus segar setiap minggu, dan satu ekstra untuk hari cuci.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Merah, Coklat, Hijau Gelap, Putih, Hitam, Army</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 2 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/kaus-hijau.png" alt="Bagian depan kaos hitam polos." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Basic Tee
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Tampil keren dengan kaus klasik kami. Cocok untuk segala kesempatan dan padu padan yang pas dengan segalanya.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Merah, Coklat, Hijau Gelap, Putih, Hitam, Navy, Army</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 3 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/mug-and-bottle.jpeg" alt="Set mug dan botol warna hijau gelap." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Mug and Bottle
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Tetap terhidrasi dan kafeinasi dengan kombinasi Mug dan Botol kami. Desain ramping dalam warna hijau gelap menambah sentuhan keanggunan pada rutinitas harian Anda.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Hijau Gelap, Putih, Hitam, Cream</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 4 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/cap-backpack-totebag.jpeg" alt="Set topi, ransel, dan tote bag warna hitam." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Topi, Ransel, Tote Bag
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Adopsi gaya hidup CEO dengan set Topi, Ransel, dan Tote Bag kami. Semuanya dalam warna hitam klasik, sempurna untuk rutinitas harian Anda.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Hitam, Putih, Cream, Navy</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 5 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/ransel-kecil.jpeg" alt="Ransel kecil dalam berbagai warna." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Ransel Kecil
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Tunjukkan gaya Anda dengan Ransel Kecil kami yang tersedia dalam warna Hijau Gelap, Putih, Hitam, Navy, dan Army. Kecil dan trendy, ini adalah aksesori yang wajib dimiliki.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Hijau Gelap, Putih, Hitam, Navy, Army</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 6 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/pulpen.png" alt="Pulpen custom dalam warna hitam." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Pulpen Custom
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Buat pernyataan dengan Pulpen Custom kami yang elegan dalam warna hitam. Aksesori yang sempurna untuk ruang kerja Anda atau sebagai hadiah yang berpikiran.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Hitam, Putih</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 7 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/note.png" alt="Notebook elegan dengan berbagai desain." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Notebook Elegan
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Catat ide-ide brilian Anda dalam Notebook Elegan kami. Desainnya yang anggun membuatnya sempurna untuk catatan sehari-hari atau hadiah spesial.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Berbagai pilihan desain</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 8 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/payung.png" alt="Payung anti-hujan dengan berbagai warna." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Payung
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Lindungi diri Anda dari hujan dengan Payung Anti-Hujan kami. Desain yang kokoh dan tahan angin untuk kenyamanan maksimal.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Berbagai warna</p>
+    </div>
+  </div>
+</div>
+
+<!-- Product 9 -->
+<div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="absolute top-2 right-2 bg-teal-400 text-white py-1 px-2 rounded-full">
+    New
+  </div>
+  <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+    <img src="/src/assets/img/bingkai-foto.png" alt="Bingkai foto dengan berbagai ukuran dan warna." class="h-full w-full object-cover object-center sm:h-full sm:w-full" />
+  </div>
+  <div class="flex flex-1 flex-col space-y-2 p-4">
+    <h3 class="text-sm font-medium text-gray-900">
+      <a href="#">
+        <span aria-hidden="true" class="absolute inset-0" />
+        Bingkai Foto
+      </a>
+    </h3>
+    <p class="text-sm text-gray-500">Hiaslah kenangan Anda dengan Bingkai Foto kami. Desain elegan dan berkualitas tinggi untuk memamerkan momen-momen istimewa dalam gaya yang unik.</p>
+    <div class="flex flex-1 flex-col justify-end">
+      <p class="text-sm italic text-gray-500">Berbagai ukuran dan warna</p>
+    </div>
+  </div>
+</div>
+
+
         </div>
       </div>
   </template>
-  
-  <script setup>
-  const products = [
-  {
-    id: 1,
-    name: 'Basic Tee 8-Pack',
-    description: 'Dapatkan rangkaian lengkap Basic Tees kami. Miliki kaus segar setiap minggu, dan satu ekstra untuk hari cuci.',
-    options: 'Merah, Coklat, Hijau Gelap, Putih, Hitam, Army',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg',
-    imageAlt: 'Delapan kaus tersusun di atas meja dalam warna hitam, zaitun, abu-abu, biru, putih, merah, kuning mustard, dan hijau.',
-  },
-  {
-    id: 2,
-    name: 'Basic Tee',
-    description: 'Tampil keren dengan kaus klasik kami. Cocok untuk segala kesempatan dan padu padan yang pas dengan segalanya.',
-    options: 'Merah, Coklat, Hijau Gelap, Putih, Hitam, Navy, Army',
-    imageSrc: '/src/assets/img/kaus-hijau.png',
-    imageAlt: 'Bagian depan kaos hitam polos.',
-  },
-  {
-    id: 3,
-    name: 'Mug and Bottle',
-    description: 'Tetap terhidrasi dan kafeinasi dengan kombinasi Mug dan Botol kami. Desain ramping dalam warna hijau gelap menambah sentuhan keanggunan pada rutinitas harian Anda.',
-    options: 'Hijau Gelap, Putih, Hitam, Cream',
-    imageSrc: '/src/assets/img/mug-and-bottle.jpeg',
-    imageAlt: 'Set mug dan botol warna hijau gelap.',
-  },
-  {
-    id: 4,
-    name: 'Topi, Ransel, Tote Bag',
-    description: 'Adopsi gaya hidup CEO dengan set Topi, Ransel, dan Tote Bag kami. Semuanya dalam warna hitam klasik, sempurna untuk rutinitas harian Anda.',
-    options: 'Hitam, Putih, Cream, Navy',
-    imageSrc: '/src/assets/img/cap-backpack-totebag.jpeg',
-    imageAlt: 'Set topi, ransel, dan tote bag warna hitam.',
-  },
-  {
-    id: 5,
-    name: 'Ransel Kecil',
-    description: 'Tunjukkan gaya Anda dengan Ransel Kecil kami yang tersedia dalam warna Hijau Gelap, Putih, Hitam, Navy, dan Army. Kecil dan trendy, ini adalah aksesori yang wajib dimiliki.',
-    options: 'Hijau Gelap, Putih, Hitam, Navy, Army',
-    imageSrc: '/src/assets/img/ransel-kecil.jpeg',
-    imageAlt: 'Ransel kecil dalam berbagai warna.',
-  },
-  {
-    id: 6,
-    name: 'Pulpen Custom',
-    description: 'Buat pernyataan dengan Pulpen Custom kami yang elegan dalam warna hitam. Aksesori yang sempurna untuk ruang kerja Anda atau sebagai hadiah yang berpikiran.',
-    options: 'Hitam, Putih',
-    imageSrc: '/src/assets/img/pulpen.png',
-    imageAlt: 'Pulpen custom dalam warna hitam.',
-  },  
-  {
-    id: 7,
-    name: 'Notebook Elegan',
-    description: 'Catat ide-ide brilian Anda dalam Notebook Elegan kami. Desainnya yang anggun membuatnya sempurna untuk catatan sehari-hari atau hadiah spesial.',
-    options: 'Berbagai pilihan desain',
-    imageSrc: '/src/assets/img/note.png',
-    imageAlt: 'Notebook elegan dengan berbagai desain.',
-  },
-  {
-    id: 8,
-    name: 'Payung',
-    description: 'Lindungi diri Anda dari hujan dengan Payung Anti-Hujan kami. Desain yang kokoh dan tahan angin untuk kenyamanan maksimal.',
-    options: 'Berbagai warna',
-    imageSrc: '/src/assets/img/payung.png',
-    imageAlt: 'Payung anti-hujan dengan berbagai warna.',
-  },
-  {
-    id: 9,
-    name: 'Bingkai Foto',
-    description: 'Hiaslah kenangan Anda dengan Bingkai Foto kami. Desain elegan dan berkualitas tinggi untuk memamerkan momen-momen istimewa dalam gaya yang unik.',
-    options: 'Berbagai ukuran dan warna',
-    imageSrc: '/src/assets/img/bingkai-foto.png',
-    imageAlt: 'Bingkai foto dengan berbagai ukuran dan warna.',
-}
-
-];
-
-  </script>
