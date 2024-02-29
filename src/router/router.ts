c75d1a69f1adf303c77c25fcfaebb66fbe85e368
Godwin-lian <godwintindaon2@gmail.com>
2024-02-29 13:28:55 +0700

    add xml sitemap

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Base from "../pages/Base.vue";
+import SiteMap from "../pages/SiteMap.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: "/",
       name: "base",
       component: Base,
+    },
+    {
+      path: '/sitemap.xml',
+      name: 'sitemap',
+      component: SiteMap,
+      meta: { contentType: 'application/xml' } // Set the response content type
     },
   ],
 });
